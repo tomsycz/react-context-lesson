1ea41782d5d7e970fd54275c4c339b7fd19475d5
Thomas Syczyk <tomsycz@gmail.com>
2021-05-05 13:22:08 +0100

    refactored cart components from redux to context based

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyDu9eMkja9fCZrVM-2-FlHpp820pxI4C6Y",
+  authDomain: "crwn-db-1ed6e.firebaseapp.com",
+  projectId: "crwn-db-1ed6e",
+  storageBucket: "crwn-db-1ed6e.appspot.com",
+  messagingSenderId: "927347222265",
+  appId: "1:927347222265:web:6a2e5f0916f662db7e6428",
+  measurementId: "G-8K8E8RYT7L",
 };
 
 firebase.initializeApp(config);
--- src/redux/store.js
+++ src/redux/store.js
@@ -14,4 +14,5 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
+// eslint-disable-next-line 
 export default { store, persistStore };
